70cb27f65440f56eb6c6d507a20e442a22bb06b9
MIKY-0 <yuyn2000@naver.com>
2026-04-14 14:29:06 +0900

    feat:20260414 상세페이지 헤더css

--- untitled/src/person.java
+++ untitled/src/person.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class person {
+    private static String name;
 
-public class practice {
+    public person(String jo) {
+    }
+
+    public void Person(String val){
+        name=val;
+    }
+    public static String get(){
+        return name;
+    }
+
+    public void print(){
+        System.out.println(name);
+    }
+
+
+
 }
