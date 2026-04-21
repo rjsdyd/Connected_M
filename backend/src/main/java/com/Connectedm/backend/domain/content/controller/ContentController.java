912e065eb61ca6f25922180ff02d595de4fe147e
dnwls0022 <yuyn2000@naver.com>
2026-04-21 17:20:54 +0900

    feat: 메인 페이지 배너 및 컨트롤러 진짜 최종 수정

--- backend/src/main/java/com/Connectedm/backend/domain/content/controller/ContentController.java
+++ backend/src/main/java/com/Connectedm/backend/domain/content/controller/ContentController.java
@@ -26,7 +26,7 @@ public class ContentController {
     /**
      * 메인페이지 데이터 조회
      */
-    @GetMapping("")
+    @GetMapping("/main")
     public ApiResponse<MainPageResponseDto> getMainPage() {
         return ApiResponse.success(contentService.getMainPageData());
     }
